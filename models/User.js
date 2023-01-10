b97ce0446bca71155794c338b320d55378dfafcd
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-01-10 15:33:50 -0600

    Added thoughts and reactions in models

--- models/User.js
+++ models/User.js
@@ -33,6 +33,10 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
 const User = model('User', userSchema);
 
 module.exports = User;
